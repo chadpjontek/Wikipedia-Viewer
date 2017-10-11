2204b2b744a887e8352c44556bff30a96a51c00f
Chad Pjontek <chadpjontek@gmail.com>
2017-10-11 06:50:48 -0400

    finished functionality and styling

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
-$(document).ready(function () {
-    $("#search-btn").on("click", function () {
-        $("<div class=\"list-group\"><a href=\"#\" class=\"list-group-item active\">Cras justo odio</a><a href=\"#\" class=\"list-group-item list-group-item-action\">Dapibus ac facilisis in</a><a href=\"#\" class=\"list-group-item list-group-item-action\">Morbi leo risus</a><a href=\"#\" class=\"list-group-item list-group-item-action\">Porta ac consectetur ac</a><a href=\"#\" class=\"list-group-item list-group-item-action disabled\">Vestibulum at eros</a></div >").appendTo(".container");
-    });
+//gloabal vars
+var endPoint = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=";
+var searchInput = "";
+//get endpoint from search box
+$("#search-btn").on("click", function () {
+    searchInput = $("#searchTerm").val();
+    endPoint += searchInput;
+    $.getJSON(endPoint, function (json) {
+        var html = "";
+        for (var i = 0; i < json[1].length; i++) {
+            html += "<a href='" + json[3][i] + "' class='list-group-item list-group-item-action bg-info text-light wow fadeInUp'>";
+            html += "<strong>" + json[1][i] + ": </strong>" + json[2][i] + "</a>"
+        }; 
+        $("#data").html(html);
+    }); 
+    endPoint = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&callback=?&search=";
 });
-
-
+// Helper function for add element box list in WOW
+WOW.prototype.addBox = function (element) {
+    this.boxes.push(element);
+  };
+  // Init WOW.js
+  new WOW().init();
